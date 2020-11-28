137bfe856ac992e66b6e50123ec5352ced53b5d2
mersede <mersede.rahbar@gmail.com>
2020-11-28 17:42:11 +0330

    add UI framework

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Alert } from 'react-bootstrap';
+import styles from './styles.less';
 
-function HomePage() {
-  return <h1>Hi</h1>;
+class HomePage extends Component {
+  render() {
+    return (
+      <>
+        <h1 className={styles.test}>hi</h1>
+        <Alert variant="success">
+          <Alert.Heading>Hey, nice to see you</Alert.Heading>
+          <p>
+            Aww yeah, you successfully read this important alert message. This example
+            text is going to run a bit longer so that you can see how spacing within an
+            alert works with this kind of content.
+          </p>
+          <hr />
+          <p className="mb-0">
+            Whenever you need to, be sure to use margin utilities to keep things nice
+            and tidy.
+          </p>
+        </Alert>
+      </>
+    );
+  }
 }
 
 export default HomePage;
