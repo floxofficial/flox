f612ddc6922ba550daca407bf3c759172cfd5c79
mersede <mersede.rahbar@gmail.com>
2020-11-29 15:00:45 +0330

    add footer

---
+++ src/components/Layout/Footer/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './styles.less';
+
+const Footer = () => (
+  <h6 className={styles.footer}>Version 1.0.1</h6>
+);
+
+export default Footer;
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from "classnames";
+import classNames from 'classnames';
 import Header from './Header';
+import Footer from './Footer';
 import styles from './styles.less';
 
 const Layout = ({ children }) => (
-  <div className={classNames('container-fluid', styles.layout)}>
-    <Header />
-    {children}
+  <div className={classNames('container-fluid', styles.layout)} id="container">
+    <div id="header">
+      <Header />
+    </div>
+    <div id="body">
+      {children}
+    </div>
+    <div id="footer">
+      <Footer />
+    </div>
   </div>
 );
 
