1ed238eb1ab70d9f6278bacfab16084e2a2b3c2e
mersede <mersede.rahbar@gmail.com>
2020-12-01 15:32:16 +0330

    add key store page

--- src/components/FileUpload/index.js
+++ src/components/FileUpload/index.js
@@ -1,52 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { useDropzone } from 'react-dropzone';
 import Button from 'Root/components/Button';
 import styles from './styles.less';
 
-const FileUpload = (props) => {
+const FileUpload = ({ setFile }) => {
+  const [fileName, setFileName] = useState('');
   const {
     getRootProps, getInputProps, open, acceptedFiles,
   } = useDropzone({
     // Disable click and keydown behavior
     noClick: true,
     noKeyboard: true,
+    multiple: false,
   });
 
-  const files = acceptedFiles.map(file => (
-    <li key={file.path}>
-      {file.path}
-      {' '}
-      -
-      {file.size}
-      {' '}
-      bytes
-    </li>
-  ));
+  useEffect(() => {
+    setFileName(acceptedFiles[0] && acceptedFiles[0].name);
+    setFile(acceptedFiles[0]);
+  }, [acceptedFiles]);
+
+  const files = acceptedFiles.map(file => <div key="image" className={styles.info}>{file.name}</div>);
 
   return (
-    <div className="container">
-      <div {...getRootProps({ className: 'dropzone' })}>
-        <input {...getInputProps()} />
-        <Button
-          onClick={open}
-          variant="primary"
-          fontWeight={500}
-          size="100%"
-        >
-          <span className={classNames('icon-upload', styles.icon)} />Select Wallet File
-        </Button>
-      </div>
-      <aside>
-        <ul>{files}</ul>
-      </aside>
+    <div>
+      {!fileName ? (
+        <div {...getRootProps({ className: 'dropzone' })}>
+          <input {...getInputProps()} />
+          <Button
+            onClick={open}
+            variant="primary"
+            fontWeight={500}
+            size="100%"
+          >
+            <span className={classNames('icon-upload', styles.icon)} />
+            Select Wallet File
+          </Button>
+        </div>
+      )
+        : (
+          <>
+            <div>{files}</div>
+            <p className={styles.message}>
+              <span className="icon-correct-circle" />
+              Your Wallet is successfully encrypted
+            </p>
+          </>
+        )
+      }
     </div>
   );
 };
 
 FileUpload.propTypes = {
-
+  setFile: PropTypes.func.isRequired,
 };
 
 export default FileUpload;
--- src/pages/KeyStore/index.js
+++ src/pages/KeyStore/index.js
@@ -1,16 +1,77 @@
 import React, { Component } from 'react';
+import { Field, Form } from 'react-final-form';
 import Title from 'Root/components/Title';
 import FileUpload from 'Root/components/FileUpload';
+import Input from 'Root/components/Input';
+import Button from 'Root/components/Button';
 import styles from './styles.less';
 
 class KeyStore extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      file: {},
+    };
+    this.onSetFile = this.onSetFile.bind(this);
+  }
+
+  onSetFile(file) {
+    this.setState({ file });
+  }
+
+  onSubmit(values) {
+    // console.warn(values);
+  }
+
+  validateForm(values) {
+    const errors = {};
+    return errors;
+  }
+
   render() {
     return (
       <div className="row justify-content-center">
         <div className="col-xl-4 col-lg-6 col-md-8 col-sm-10 col-11">
           <Title text="Unlock with keystore file" mt={48} link="/" />
           <div className={styles.upload}>
-            <FileUpload />
+            <FileUpload setFile={this.onSetFile} />
+            {this.state.file && this.state.file.name && (
+            <>
+              <Form
+                onSubmit={values => this.onSubmit(values)}
+                validate={values => this.validateForm(values)}
+                render={({
+                  submitError, handleSubmit, submitting,
+                }) => (
+                  <form className={styles.form} onSubmit={handleSubmit}>
+                    <label className="label-primary">Password</label>
+                    <Field name="password">
+                      {({ input, meta }) => (
+                        <Input
+                          type="password"
+                          placeholder="Enter your password here"
+                          variant="pass"
+                          input={input}
+                          meta={meta}
+                        />
+                      )}
+                    </Field>
+                    {submitError && <div className="error">{submitError}</div>}
+                    <Button
+                      type="submit"
+                      content="Unlock wallet"
+                      variant="primary"
+                      size="100%"
+                      fontWeight={500}
+                      className="mt-4"
+                      disabled={submitting}
+                    />
+                  </form>
+                )}
+              />
+            </>
+            )}
           </div>
         </div>
       </div>
