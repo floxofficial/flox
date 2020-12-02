a0b3d864ae6585fad5263bde233f367fa6b94390
mersede <mersede.rahbar@gmail.com>
2020-12-02 13:45:39 +0330

    add wallet info page

--- src/pages/WalletInfo/index.js
+++ src/pages/WalletInfo/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Title from 'Root/components/Title';
 import CopyText from 'Root/components/CopyText';
 import styles from './styles.less';
+import Button from '../../components/Button';
 
 class WalletInfo extends Component {
   render() {
@@ -9,7 +10,7 @@ class WalletInfo extends Component {
     const key = '0x4b8276fc8003a89fe2a0ad9de26ca82c2e26cb3339877b487662bd7219797a9e';
     return (
       <div className="row justify-content-around">
-        <div className="col-xl-5 col-lg-6 col-md-8 col-sm-10 col-11">
+        <div className="col-xl-6 col-lg-6 col-md-8 col-sm-10 col-11 px-4">
           <Title text="Your wallet info" mt={48} />
           <p className={styles.message}>
             Do not lose your private key! There is no way to recover lost keys.
@@ -26,6 +27,11 @@ class WalletInfo extends Component {
               <CopyText text={key} icon />
             </p>
           </div>
+          <a className={styles.download} href="../../assets/images/logo.png" download="logo.png">
+            <div className={styles['download-text']}>Download Keystore file</div>
+            <div className="icon-upload" />
+          </a>
+          <Button variant="primary" content="Continue" size="118px" />
         </div>
       </div>
     );
