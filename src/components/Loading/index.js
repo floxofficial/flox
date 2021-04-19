af2031bb9fa3757455e646d8b203bcb457d169a3
mersede <mersede.rahbar@gmail.com>
2021-04-19 14:08:41 +0430

    add lottie player

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import classNames from 'classnames';
-import logo256 from 'Root/assets/images/Flox - logo motion - 256x256.gif';
-import logo128 from 'Root/assets/images/Flox - logo motion - 128x128.gif';
+import LogoMotion from 'Root/components/LogoMotion';
 import styles from './styles.less';
 
 const Loading = ({ size = 128, isFull }) => (
   <div className={classNames(styles.container, isFull && styles.position)}>
-    <img src={logo128} alt="loading" height={size} width={size} />
+    <div style={{ width: `${size}px`, height: `${size}px` }}>
+      <LogoMotion />
+    </div>
   </div>
 );
 
