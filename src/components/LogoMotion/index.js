15fd95dad34df8b317f63250291fad6e2f41a657
mersede <mersede.rahbar@gmail.com>
2021-04-22 10:33:42 +0430

    replace lottie package

--- src/components/LogoMotion/index.js
+++ src/components/LogoMotion/index.js
@@ -1,37 +1,11 @@
-import React, { useState } from 'react';
-import Lottie from 'lottie-web-react';
+import React from 'react';
+import Lottie from 'lottie-react';
+import watchAnimationData from '../../assets/json/Flox - logo motion - 128x128.json';
 
-const watchAnimationData = require('../../assets/json/Flox - logo motion - 128x128.json');
-
-const renderer = 'svg';
-const rendererSettings = {
-  preserveAspectRatio: 'xMinYMin slice',
-};
-
-const LogoMotion = () => {
-  const [playingState, setPlayingState] = useState('play');
-  const [autoplay, setAutoplay] = useState(false);
-  const [loop, setLoop] = useState(true);
-  const [animationData, setAnimationData] = useState(watchAnimationData);
-  const [speed, setSpeed] = useState(1);
-  const [direction, setDirection] = useState(1);
-
-  return (
-    <div>
-      <Lottie
-        options={{
-          renderer,
-          loop,
-          autoplay,
-          animationData,
-          rendererSettings,
-        }}
-        playingState={playingState}
-        speed={speed}
-        direction={direction}
-      />
-    </div>
-  );
-};
+const LogoMotion = () => (
+  <div>
+    <Lottie animationData={watchAnimationData} />
+  </div>
+);
 
 export default LogoMotion;
