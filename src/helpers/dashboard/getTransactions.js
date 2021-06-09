0504e5a448ce7cf2f1ff3e5d4bd07968158de6b1
Matin Kaboli <matinkaboli@aol.com>
2021-06-10 00:03:51 +0430

    fix: show trx

--- src/helpers/dashboard/getTransactions.js
+++ src/helpers/dashboard/getTransactions.js
@@ -7,10 +7,14 @@ import currentExplorer from 'Root/helpers/currentExplorer';
 export default async (account) => {
   try {
     const transactions = await fetch(
-      `${currentExplorer()}/v1/transaction?accountAddress=${account.address}&limit=10&skip=0`,
+      `${currentExplorer()}/v1/transaction?accountAddress=${
+        account.address
+      }&limit=10&skip=0`,
     ).then((res) => res.json());
 
-    if (transactions.list < 10000000) {
+    console.log(transactions);
+
+    if (transactions.total < 10000000) {
       store.dispatch({
         type: types.transactions.LOAD,
         payload: transactions.list,
